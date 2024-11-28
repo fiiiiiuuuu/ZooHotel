e673b7fc50f019e849f7a7e36e4eb0ac0d95ba3f
Niebieski <157232546+fiiiiiuuuu@users.noreply.github.com>
2024-11-28 21:28:39 +0300

    Добавил footer

--- js/script.js
+++ js/script.js
@@ -8,3 +8,21 @@ document.querySelectorAll('.dropdown').forEach((dropdown) => {
         if (menu) menu.style.display = 'none';
     });
 });
+
+window.addEventListener('scroll', function() {
+    var toTop = document.getElementById('to-top');
+
+    if (this.pageYOffset > 200) {
+        toTop.style.display = 'inline-block';
+    } else {
+        toTop.style.display = 'none';
+    }
+});
+
+document.getElementById('to-top').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
